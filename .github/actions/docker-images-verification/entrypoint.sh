6e5918f2c868ebbd3c5598b6f44bde7365b800c2
Huijun Zhu <hzhu16@unl.edu>
2019-12-02 14:51:49 -0600

    workflow updated

--- .github/actions/docker-images-verification/entrypoint.sh
+++ .github/actions/docker-images-verification/entrypoint.sh
@@ -4,4 +4,4 @@
 #time=$(date)
 echo "Hello"
 #echo ::set-output name=time::$time
-./docker-images-verify --images-file ./docker_images.txt
+python docker-images-verify --images-file ./docker_images.txt
